2ad9cbaf40d529bf572c42428c103a00aaf109d5
Jonathan <infiniteSako@gmail.com>
2024-08-07 01:47:13 -0700

    Added login and signup loading text

--- src/components/auth-btn.tsx
+++ src/components/auth-btn.tsx
@@ -2,16 +2,22 @@
 
 import { useFormStatus } from "react-dom";
 import { Button } from "./ui/button";
+import { ReloadIcon } from "@radix-ui/react-icons";
+
 
 type AuthBtnProps = {
   type: "signUp" | "logIn";
 };
 
+
+
 export default function AuthBtn({ type }: AuthBtnProps) {
+  const loadingText = type === "signUp" ? "Signing up" : "Logging in"
   const { pending } = useFormStatus();
   return (
-    <Button disabled={pending}>
-      {type === "signUp" ? "Sign Up" : "Log In"}
+    <Button disabled={pending} className="flex gap-1">
+      {pending ? <ReloadIcon className="animate-spin" /> : null}
+      {pending ? loadingText : (type === "signUp" ? "Sign Up" : "Log In")}
     </Button>
   );
 }
